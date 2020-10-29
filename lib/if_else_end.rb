32670a89c5b9d8667d614e040a5da006352091b7
Fem Adisa <femiwonderful@gmail.com>
2020-10-29 16:07:04 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
 # Write your solution here
+chanceRain = 0
+if chanceRain <= 0.25
+  puts "PACLK a sun shelter"
+elsif (chanceRain > 0.25 && chanceRain < 0.75)
+  puts "Paxk umvrella"
+else
+  puts "Enjoy your staycation!"
+end 
+#puts "Oh, and always a sunscreen"
