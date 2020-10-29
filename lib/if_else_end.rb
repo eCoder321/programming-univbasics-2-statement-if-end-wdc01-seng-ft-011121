cd241e6670789aaaa3182cbd1e4fdacef984f6aa
Fem Adisa <femiwonderful@gmail.com>
2020-10-29 16:24:42 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,5 @@
 # Write your solution here
-current_second = Time.now.select_second
+current_second = Time.now.to_i
 if chanceRain <= 0.25
   puts "PACLK a sun shelter"
 elsif (chanceRain > 0.25 && chanceRain < 0.75)
